c257c538679aa55604d3fca367a018feed491cbc
Rizka Nugraha <68932997+rizka32@users.noreply.github.com>
2022-09-05 20:53:25 +0700

    tes
    tes

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   cout << "Nama : Rizka Nugraha" << endl;
+   cout << "NIM : A11.2022.14119" << endl;
+   cout << "Alamat : Jln. RE Martadinata, Pandean, Proyonanggan Utara, Kec. Batang, Kab. Batang" << endl;
+   cout << "Asal Sekolah : SMAN 2 Batang" << endl;
+
+       return 0;
+}
