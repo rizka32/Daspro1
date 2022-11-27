396ec8d4b7c4a4f25ee8a4075fdbb5a745ab6e38
Rizka Nugraha <68932997+rizkanugrha@users.noreply.github.com>
2022-11-27 13:08:53 +0700

    updt
    updt

--- pertemuan 17/rizka.cpp
+++ pertemuan 17/rizka.cpp
@@ -4,8 +4,18 @@ using namespace std;
 
 int main () {
 
-int i, j, a=0, angka, mins, maks, b=0, sum, c, z, p=0, m, n=0;
+int i, j, a=0, angka, mins, maks, b=0, has, c, z, p=0, m, n=0;
 float ra, f;
+string k[9] = {"Air Mendidih",
+                "Air Mandi Panas",
+                "Temperatur Tubuh",
+                "Cuaca Pantai",
+                "Temperatur Ruangan",
+                "Hari Yang Dingin",
+                "Titik Beku Air",
+                "Cuaca Dingin Bersalju",
+                "Cuaca Sangat Dingin Bersalju"
+};
 
 //soal no 1
 cout << "------------------" << endl;
@@ -88,8 +98,6 @@ cout << endl;
 cout << "Max = " << maks << endl;
 cout <<"Min = " << mins << endl;
 
-
-
 //soal no 7
 cout << "------------------" << endl;
 cout << "Soal no. 7" << endl;
@@ -103,12 +111,9 @@ for(j = 1; ; j++) {
     cout << z << endl;
     p = p + z;
 }
-
 cout << "Jumlah angka = " << p << endl;
 
-
-
-// no 8
+//soal no 8
 cout << "\n------------------" << endl;
 cout << "Soal no. 8" << endl;
 cout << "------------------" << endl;
@@ -121,9 +126,7 @@ for(j = 0; ; j++) {
     cout << angka << endl;
     a += j;
 }
-
 cout << "Jumlah angka = " << j << endl;
-
 
 // soal no 9
 cout << "\n------------------" << endl;
@@ -138,78 +141,73 @@ for(j = 0; ; j++) {
     } else if(m != 9999) {
         n += m;
     }
-    b += 1;
+        b += 1;
 }
-
 cout << "Jumlah angka = " << n << endl;
-cout << "Jumlah angka cacah = " << b << endl;
+cout << "Jumlah cacah angka = " << b << endl;
 
 // soal no 10
-
 cout << "\n------------------" << endl;
 cout << "Soal no. 10" << endl;
 cout << "------------------" << endl;
 for(i = 1; i <= 7; i++) {
-
-    cout << "Looping yang ke - " <<  i<< endl;
+    cout << "Looping yang ke - " << i << endl;
     for(j = 1; j <= 10; j++) {
-        cout << "-angka " << j << "-"<< endl;
+        cout << "-angka " << j << "-" << endl;
     }
+    cout << endl;
 }
 
 //soal no 11
-
 cout << "\n------------------" << endl;
 cout << "Soal no. 11" << endl;
 cout << "------------------" << endl;
 cout << "Masukan angka = ";
 cin >> angka;
 for(i = 1; i <= 10; i++) {
-    sum = i*angka;
-   cout << i << " x " << angka << " = " << sum<<  endl;
+    has = i*angka;
+    cout << i << " x " << angka << " = " << has << endl;
 }
 
 //soal no 12
-
 cout << "\n------------------" << endl;
 cout << "Soal no. 12" << endl;
 cout << "------------------" << endl;
-  cout << "-------------------------" << endl;
-  cout << "Celcius     Fahremheit" << endl;
-  cout << "-------------------------" << endl;
+cout << "-------------------------" << endl;
+cout << "Celcius\tFahrenheit" << endl;
+cout << "-------------------------" << endl;
 for(c = 0; c <= 100; c++) {
-
   f = ((9.0/5)*c)+32;
-  printf("%d \t|\t %.1f |\n",c, f);
+  printf("%d \t|\t %.1f |\n", c, f);
 }
 
-
 // soal no 13
-
-  cout << "------------------------------------------------------------------------------" << endl;
-  cout << "Celcius \t Fahremheit \t Keterangan" << endl;
-  cout << "------------------------------------------------------------------------------" << endl;
+cout << "\n------------------" << endl;
+cout << "Soal no. 13" << endl;
+cout << "------------------" << endl;
+cout << "------------------------------------------------------------------------------" << endl;
+cout << "Celcius \t Fahrenheit \t Keterangan" << endl;
+cout << "------------------------------------------------------------------------------" << endl;
 for(c = 100; c >= -45; c--) {
   f = ((9.0/5)*c)+32;
   if(c == 100) {
-  printf("%d \t\t %.1f \t\t Air Mendidih \n",c, f);
+  printf("%d \t\t %.1f \t\t %s \n", c, f, k[0].c_str());
   } else if(c == 40) {
-  printf("%d \t\t %.1f \t\t Air Mandi Panas \n",c, f);
+  printf("%d \t\t %.1f \t\t %s \n", c, f, k[1].c_str());
   } else if(c == 37) {
-  printf("%d \t\t %.1f \t\t Temperatur Tubuh \n",c, f);
+  printf("%d \t\t %.1f \t\t %s \n", c, f, k[2].c_str());
   } else if(c == 30) {
-  printf("%d \t\t %.1f \t\t Cuaca Pantai\n",c, f);
+  printf("%d \t\t %.1f \t\t %s \n", c, f, k[3].c_str());
   } else if(c == 21) {
-  printf("%d \t\t %.1f \t\t Temperatur Ruangan \n",c, f);
+  printf("%d \t\t %.1f \t\t %s \n", c, f, k[4].c_str());
 } else if(c == 10) {
-  printf("%d \t\t %.1f \t\t Hari Yang Dingin \n",c, f);
+  printf("%d \t\t %.1f \t\t %s \n", c, f, k[5].c_str());
 } else if(c == 0) {
-  printf("%d \t\t %.1f \t\t Titik Beku Air \n",c, f);
+  printf("%d \t\t %.1f \t\t %s \n", c, f, k[6].c_str());
 } else if(c == -18) {
-  printf("%d \t\t %.1f \t\t Cuaca Dingin Bersalju \n",c, f);
+  printf("%d \t\t %.1f \t\t %s \n", c, f, k[7].c_str());
 } else if(c == -40) {
-  printf("%d \t\t %.1f \t\t Cuaca Sangat Dingin Bersalju \n",c, f);
-
+  printf("%d \t\t %.1f \t\t %s \n", c, f, k[8].c_str());
   cout << "------------------------------------------------------------------------------" << endl;
 }
 }
