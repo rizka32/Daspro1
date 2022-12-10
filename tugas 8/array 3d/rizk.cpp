74ab69b02c26efb1ad64e7d6e57c3ccc306e52fd
Rizka Nugraha <68932997+rizkanugrha@users.noreply.github.com>
2022-12-10 22:42:28 +0700

    Update rizk.cpp

--- tugas 8/array 3d/rizk.cpp
+++ tugas 8/array 3d/rizk.cpp
@@ -3,24 +3,24 @@
 int main() {
 
 float rata_rata;
-int love = 0, you = 1, cher = 0, ly = 2;
+int love = 0, you = 1, co = 0, mel = 2;
 int hm[3][2][2];
 
 printf("Array kelipatan 4 = ");
 for(int o = 0; o < 3; o++) {
     for(int k = 0; k < 2; k++) {
         for(int e = 0; e < 2; e++){
-            hm[o][k][e] = ly;
-            ly += 2;
-            printf("%d " "", ly);
-            cher += ly;
+            hm[o][k][e] = mel;
+            mel += 2;
+            printf("%d " "", mel);
+            co += mel;
             love += you;
-            rata_rata = (float)cher / love;
+            rata_rata = (float)co / love;
         }
     }
 }
 printf("\n");
-printf("Sum kelipatan 4 = %d\n", cher);
+printf("Sum kelipatan 4 = %d\n", co);
 printf("Rata-rata kelipatan 4 = %.2f\n", rata_rata);
 
 return 0;
